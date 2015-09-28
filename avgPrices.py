fec7cb3d2e52f0acc9bdd3b7d1ba43059365b10b
Justin Hill <justin@JKH-MBP.local>
2015-09-28 18:57:33 -0500

    added dict and class examples to avgPrices

--- avgPrices.py
+++ avgPrices.py
@@ -4,12 +4,37 @@ class Product(object):
         self.prices = []
         
     def averagePrice(self):
-        total = sum(self.prices)
-        return total / len(self.prices)        
+        return doAverage(self.prices)      
 
+
+def doAverage(prices):
+    total = sum(prices)
+    return total / len(prices)
+    
+
+def print_blank_lines(num_lines):
+    for i in range(0,num_lines):
+        print ''
 
 def main():
 
+    print_blank_lines(1)
+
+    
+    # Dictionary
+    products = {}
+    products['chips'] = [2,3,4,5]
+    products['bread'] = [3,4,5,6]
+    products['candy'] = [4,5,6,7]
+    
+    for name, prices in products.iteritems():
+        avg = doAverage(prices)
+        print 'DICTIONARY: Average price of {} is {}'.format(name, avg)
+
+
+    print_blank_lines(2)
+    
+    # Class
     products = []
 
     p = Product()
@@ -22,10 +47,10 @@ def main():
     p.prices = [6,7,8,9]
     products.append(p)
 
-
     for p in products:
-        print 'Average price of {} is {}'.format(p.name, p.averagePrice())
-    
+        print 'CLASS and LIST: Average price of {} is {}'.format(p.name, p.averagePrice())
+        
+    print_blank_lines(1)
     
 if __name__ == "__main__":
     main()  
